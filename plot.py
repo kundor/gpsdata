7d39bea46cac15db83e71ced4ecb30ce5e13d442
Nick Matteo <kundor@kundor.org>
2016-10-28 09:30:52 -0600

    Plot functions scratchpad

---
+++ plot.py
@@ -0,0 +1,106 @@
+import matplotlib as mp
+from matplotlib import pyplot as plt
+import numpy as np
+from math import floor, ceil
+
+def posneg(arr):
+    """Check that input consists of some positive entries followed by negative entries,
+    with no mixing. Return index of first negative entry."""
+    neg = False
+    for i, k in enumerate(arr):
+        if neg and k > 0:
+            print("{} (entry {}) is positive, after negative entry at {}!".format(k, i, ind))
+            return ind
+        if not neg and k < 0:
+            ind = i
+            neg = True
+    return ind
+    
+def dorises(snrdata, prn):
+    _, el, az, sod, snr = zip(*(r for r in snrdata if r.prn == prn))
+    riz = rises(el, sod)
+    for i, (rb, re) in enumerate(riz):
+        plt.subplot(len(riz), 1, i+1)
+        plt.scatter(el[rb:re+1], snr[rb:re+1], s=1)
+        plt.xlim(el[rb], el[re])
+        plt.xlabel('Elevation')
+        plt.ylabel('SNR')
+        plt.title('PRN {}, DoY {}, {:02d}:{:02d}--{:02d}:{:02d}, Az {}--{}'.format(
+                   prn, snrdata.doy, int(sod[rb]//3600), int(sod[rb] % 3600)//60,
+                   int(sod[re]//3600), int(sod[re] % 3600)//60,
+                   min(az[rb:re+1]), max(az[rb:re+1])))
+    plt.tight_layout()
+
+
+def rises(el, sod):
+    difel = np.diff(el)
+    starts = [0] + np.argwhere(np.diff(sod) > 100).ravel().tolist() + [-1]
+    riz = []
+    for beg, end in zip(starts, starts[1:]):
+        peak = posneg(difel[beg:end])
+        riz.append([beg+1, beg+peak])
+    return riz
+
+def snrVSel(snrdata, prn, secstart = 0, secend = 86400, color = None):
+    snr = [r.snr for r in snrdata if r.prn == prn and secstart < r.sod < secend]
+    el = [r.el for r in snrdata if r.prn == prn and secstart < r.sod < secend]
+    plt.scatter(el, snr, s = 1, color = color)
+#    plt.xlim(min(el), max(el))
+#    plt.xlabel('Elevation')
+#    plt.ylabel('SNR')
+#    plt.title('PRN {}, DoY {}'.format(prn, snrdata.doy))
+    plt.tight_layout()
+
+def iterSNRs(SNRs):
+    for snr in SNRs:
+        for rec in snr:
+            yield rec.az, rec.el, rec.snr
+
+def itergdo(gdo):
+    for r in gdo.iterlist(obscode=('az','el' 'S1')):
+        for rec in r:
+            yield rec
+
+def azelbin(iterfn, dat, scale = 2):
+    snravg = np.zeros((360*scale, 90*scale))
+    snrnum = np.zeros((360*scale, 90*scale))
+    snrstd = np.zeros((360*scale, 90*scale))
+    for az,el,snr in iterfn(dat):
+        azi = floor(az*scale)
+        eli = floor(el*scale)
+        n = snrnum[azi, eli]
+        snravg[azi,eli] = n/(n+1)*snravg[azi,eli] + 1/(n+1)*snr
+        snrnum[azi,eli] += 1
+    for az,el,snr in iterfn(dat):
+        azi = floor(az*scale)
+        eli = floor(el*scale)
+        snrstd[azi, eli] += (snr - snravg[azi, eli])**2
+    snrstd = np.sqrt(snrstd / np.where(snrnum > 0, snrnum, 1))
+    snravg = np.ma.masked_where(snrnum == 0, snravg)
+    snrstd = np.ma.masked_where(snrnum == 0, snrstd)
+    plotazel(snravg, 'Mean SNR', scale)
+    plotazel(snrstd, 'SNR standard deviation', scale)
+    return snravg, snrnum, snrstd
+
+def plotazel(dat, title, scale = 4):
+    plt.figure()
+    vmin = np.percentile(dat.compressed(),1)
+    vmin = max(floor(vmin), np.min(dat))
+    vmax = np.percentile(dat.compressed(),99)
+    vmax = min(ceil(vmax), np.max(dat))
+    plt.pcolormesh(dat.T, vmin=vmin, vmax=vmax)
+    axx = plt.gca()
+    box = axx.get_position()
+    xloc, xlab = plt.xticks()
+    plt.xticks(xloc, xloc/scale)
+    yloc, ylab = plt.yticks()
+    plt.yticks(yloc, yloc/scale)
+    plt.xlim(0, 360*scale)
+    plt.ylim(10*scale, 90*scale)
+    plt.title(title)
+    plt.xlabel('Azimuth')
+    plt.ylabel('Elevation')
+    axc = plt.axes([box.x0 + box.width * 1.05, box.y0, 0.01, box.height])
+    plt.colorbar(cax = axc)
+
+
