3de0cf71a4dbe5bc6e16a1859dd31ec114ca1d8a
Nick Matteo <kundor@kundor.org>
2016-11-07 16:19:13 -0700

    Extend dorises2 to do falling arcs also

--- plot.py
+++ plot.py
@@ -25,7 +25,7 @@ def dorises(snrdata, prn):
     """
     _, el, az, sod, snr = zip(*(r for r in snrdata if r.prn == prn))
     riz = rises(el, sod)
-    for i, (rb, re) in enumerate(riz):
+    for i, (rb, re, _) in enumerate(riz):
         plt.subplot(len(riz), 1, i+1)
         plt.scatter(el[rb:re+1], snr[rb:re+1], s=1)
         plt.xlim(el[rb], el[re])
@@ -50,26 +50,45 @@ def dorises2(snrdata, prn, doy0):
     except ValueError:
         print('PRN {} not found'.format(prn))
         return
-    riz = rises(el, sow, prn)
-    for i, (rb, re) in enumerate(riz):
-        fig = plt.figure()
-        plt.scatter(el[rb:re+1], snr[rb:re+1], s=1)
-        plt.xlim(el[rb], el[re])
+    eles = np.array(el)
+    azis = np.array(az)
+    riz = rises(eles, sow, prn)
+    for beg, peak, end in riz:
+        fig = plt.figure(figsize=(16,6))
+        gs = mp.gridspec.GridSpec(1, 2, width_ratios=[4,1])
+        ax0 = plt.subplot(gs[0])
+        pel = eles[peak]
+        twoel = 2*pel
+        ax0.scatter(eles[beg:peak+1], snr[beg:peak+1], s=1)
+        ax0.scatter(twoel - eles[beg:peak+1], snr[beg:peak+1], s=1)
+        xt = np.arange(pel/5, twoel-1, pel/5)
+        xlab = ['{:.0f}°'.format(x if x <= pel else twoel - x) for x in xt]
+        plt.xticks(xt, xlab)
+        plt.xlim(floor(eles[beg]), ceil(twoel - eles[end]))
         plt.xlabel('Elevation')
         plt.ylabel('SNR')
-        doy = int(doy0 + sow[rb]//86400)
-        minaz = min(az[rb:re+1])
-        maxaz = max(az[rb:re+1])
-        if maxaz > 350 and minaz < 10:
-            minaz = min(a for a in az[rb:re+1] if a > 180)
-            maxaz = max(a for a in az[rb:re+1] if a < 180)
-        plt.title('PRN {}, DoY {}, {}--{}, Az {:.0f}--{:.0f}'.format(
-            prn, doy, sowhrmin(sow[rb]), sowhrmin(sow[re]), minaz, maxaz))
+        doy = int(doy0 + sow[beg]//86400)
+        daz = np.diff(az[beg:end+1])
+        if (daz >= 0).all():
+            azinc = r'$\uparrow$'
+        elif (daz <= 0).all():
+            azinc = r'$\downarrow$'
+        elif np.count_nonzero(daz < 0) == 1 and daz[daz < 0] < -350:
+            azinc = r'$\uparrow$'
+        elif np.count_nonzero(daz > 0) == 1 and daz[daz > 0] > 350:
+            azinc = r'$\downarrow$'
+        else:
+            print('Azimuths are not monotone.')
+            azinc = ''
+        plt.title('PRN {}, DoY {}, {}--{}, Az {:.0f}--{:.0f} {}'.format(
+            prn, doy, sowhrmin(sow[beg]), sowhrmin(sow[peak]), az[beg], az[end], azinc))
+        ax1 = plt.subplot(gs[1], projection='polar')
+        polarazel(azis[beg:end+1], eles[beg:end+1], ax1)
         plt.tight_layout()
-        quart = int(5 - az[rb] // 90)  # rising quarter
+        quart = int(5 - az[beg] // 90)  # rising quarter
         if quart == 5:
             quart = 1
-        hr = (int(sow[rb]) % 86400) // 3600
+        hr = (int(sow[beg]) % 86400) // 3600
         plt.savefig('{:02d}-{}-{:02d}-Q{}.png'.format(prn, doy, hr, quart))
         plt.close(fig)
 
@@ -104,20 +123,53 @@ def rises(el, sod, prn=None):
             print('Max elevation {}, PRN {}, {} to {} ({}--{})'.format(
                 el[peak], prn, beg+1, end, sowdhrmin(sod[beg+1]), sowhrmin(sod[end])))
             continue
-        riz.append([beg+1, peak])
+        riz.append([beg+1, peak, end])
     return riz
+    
+    
+def add_arrow(line, start_ind=None, direction='right', size=15, color=None):
+    """Add an arrow to a line.
 
-def polarazel(azis, eles):
+    line:       Line2D object
+    position:   x-position of the arrow. If None, mean of xdata is taken
+    direction:  'left' or 'right'
+    size:       size of the arrow in fontsize points
+    color:      if None, line color is taken.
+    """
+    # from http://stackoverflow.com/a/34018322/2132213
+    if color is None:
+        color = line.get_color()
+
+    xdata = line.get_xdata()
+    ydata = line.get_ydata()
+
+    if start_ind is None:
+        start_ind = len(xdata) // 2
+    if direction == 'right':
+        end_ind = start_ind + 1
+    else:
+        end_ind = start_ind - 1
+
+    line.axes.annotate('',
+        xytext=(xdata[start_ind], ydata[start_ind]),
+        xy=(xdata[end_ind], ydata[end_ind]),
+        arrowprops={'arrowstyle': '->', 'color': color},
+        size=size)
+
+def polarazel(azis, eles, ax=None):
     """Plot azimuth and elevations (in degrees) as a curve on a polar plot.
     
     Input should be numpy arrays."""
-    plt.figure()
-    plt.polar((90 - azis)/180 * pi, 90 - eles)
+    if ax is None:
+        plt.figure()
+        ax = plt.subplot(1,1,1, projection='polar')
+    line = ax.plot((90 - azis)/180 * pi, 90 - eles)
     xloc, _ = plt.xticks()
-    xlab = [str(int(x * 180 / pi)) + '°' for x in xloc]
-    plt.xticks(xloc, xlab)
+    xlab = (90 - xloc * 180 / pi) % 360
+    plt.xticks(xloc, ['{:.0f}°'.format(x) for x in xlab])
     yloc = range(20, 90, 20)
     plt.yticks(yloc, (str(90 - y) for y in yloc))
+    add_arrow(line[0])
 
 def snrVSel(snrdata, prn, secstart=0, secend=86400, color=None):
     snr = [r.snr for r in snrdata if r.prn == prn and secstart < r.sod < secend]
@@ -157,11 +209,11 @@ def azelbin(iterfn, dat, scale=2):
     snrstd = np.sqrt(snrstd / np.where(snrnum > 0, snrnum, 1))
     snravg = np.ma.masked_where(snrnum == 0, snravg)
     snrstd = np.ma.masked_where(snrnum == 0, snrstd)
-    plotazel(snravg, 'Mean SNR', scale)
-    plotazel(snrstd, 'SNR standard deviation', scale)
+    plotazelbin(snravg, 'Mean SNR', scale)
+    plotazelbin(snrstd, 'SNR standard deviation', scale)
     return snravg, snrnum, snrstd
 
-def plotazel(dat, title, scale=2):
+def plotazelbin(dat, title, scale=2):
     plt.figure()
     vmin = np.percentile(dat.compressed(), 1)
     vmin = max(floor(vmin), np.min(dat))
